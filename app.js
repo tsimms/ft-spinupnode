3e77951f2c96e31afd2af7aff532e077baa5505e
tsimms <tim.simms@influentmetrics.com>
2024-03-08 02:45:42 -0500

    first_level

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import container from './container.js';
 
 const server = await container({
-    staticFiles: [
-        'https://raw.githubusercontent.com/tsimms/ft-spinupnode/init/server/server.js',
-        'https://raw.githubusercontent.com/tsimms/ft-spinupnode/init/server/package.json'
-    ]
+  staticFiles: [
+    'https://raw.githubusercontent.com/tsimms/ft-spinupnode/init/server/server_hello.js',
+    'https://raw.githubusercontent.com/tsimms/ft-spinupnode/init/server/server_chat.js',
+    'https://raw.githubusercontent.com/tsimms/ft-spinupnode/init/server/client_chat.html',
+    'https://raw.githubusercontent.com/tsimms/ft-spinupnode/init/server/package.json'
+  ]
 });
 const { webcontainerInstance } = server;
